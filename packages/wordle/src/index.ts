fddfea891b2898d67287792a0b50af3baa23f63f
Maiko Tan <maiko.tan.coding@gmail.com>
2023-09-12 20:49:31 +0800

    chore: fix pipeline for yakumo

--- packages/wordle/src/index.ts
+++ packages/wordle/src/index.ts
@@ -4,8 +4,16 @@ import { Context } from 'koishi'
 export const name = 'koishi-plugin-wordle'
 
 export class Wordle extends WordleCore {
-  getTodayWord(): string {}
-  validateInput(): WordleCore.Validation {}
+  getTodayWord(): string {
+    return ''
+  }
+  validateInput(): WordleCore.Validation {
+    return {
+      isValid: false,
+      color: [],
+      
+    }
+  }
 }
 
 export function apply(ctx: Context) {
