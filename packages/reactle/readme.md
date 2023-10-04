c6cd3cb76d0a40f9fc2ba7db15ecd4c059b7aa17
Maiko Tan <maiko.tan.coding@gmail.com>
2023-10-04 23:30:51 +0800

    feat: add new plugin reactle

---
+++ packages/reactle/readme.md
@@ -0,0 +1,25 @@
+<p align="center">
+
+![header](https://github.com/koishijs/wordle/blob/master/assets/header.png)
+
+</p>
+
+<h1 align="center"><a href="https://wordle.koishi.chat/plugins/reactle.html">koishi-plugin-reactle</a></h1>
+
+<p align="center">
+在 Koishi 中游玩开源版 Wordle！
+</p>
+
+<p align="center">
+<img src="https://img.shields.io/npm/v/koishi-plugin-reactle?style=flat-square" alt="NPM">
+<img src="https://img.shields.io/github/actions/workflow/status/koishijs/wordle/lint.yml?style=flat-square&amp;label=Lint" alt="Github Action Lint Workflow">
+<img src="https://img.shields.io/github/actions/workflow/status/koishijs/wordle/test.yml?style=flat-square&amp;label=Test" alt="Github Action Test Workflow">
+</p>
+
+## 使用
+
+详见[文档](https://wordle.koishi.chat/plugins/reactle.html)
+
+## 许可证
+
+本插件使用 [AGPL-3.0](https://github.com/koishijs/wordle/tree/master/LICENSE) 许可证发布。
---
+++ packages/reactle/src/index.ts
@@ -0,0 +1,37 @@
+import { defineVariation } from '@koishijs/wordle'
+import { Schema } from 'koishi'
+
+import wordlist from './data/wordlist.json'
+
+export interface Config {}
+
+export default defineVariation<Config>({
+  name: 'koishi-plugin-reactle',
+  command: 'reactle',
+  Config: Schema.intersect([]).i18n({
+    'zh-CN': require('./locales/schema.zh-CN'),
+  }) as Schema<Config>,
+  locales: {
+    'zh-CN': require('./locales/zh-CN'),
+  },
+  validWords: wordlist.map((word: string) => word.split('')),
+  init(command) {
+    command.option('random', '-r')
+  },
+  async getCurrentWord({ options }) {
+    if ((options as any).random) {
+      return wordlist[Math.floor(Math.random() * wordlist.length)].split('')
+    }
+    // 1 January 2022 Game Epoch
+    const daysSinceLaunch = (Number(new Date()) - Number(new Date(2022, 0))) / 1000 / 60 / 60 / 24
+    return wordlist[Math.floor(daysSinceLaunch) % wordlist.length].split('')
+  },
+})
+
+export interface NYTimesWordleResponse {
+  id: number
+  solution: string
+  print_date: string
+  days_since_launch: number
+  editor: string
+}
